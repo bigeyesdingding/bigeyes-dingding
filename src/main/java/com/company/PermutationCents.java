4a79d1de5cdfcd31db01f3a084212a29e251d517
Ding Han <handing@evestemptation.com>
2017-08-17 18:29:53 -0700

    some changes in JUnit4, should work now

---
+++ src/main/java/com/company/PermutationCents.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PermutationCents {
+
+    public static void
+
+    public static void main(String[] args) {
+        // write your code here
+
+        String a = new String("123456");
+        String b = new String("123456");
+
+        System.out.println(a.equals(b));
+    }
+}
