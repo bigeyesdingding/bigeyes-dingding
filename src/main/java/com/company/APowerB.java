e4ee6880585861fae175c3cdd2aa54ffb4b7d86f
hannah-ding <dinghan1993@gmail.com>
2017-09-07 16:01:09 -0700

    recursion II

---
+++ src/main/java/com/company/APowerB.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class APowerB {
+
+
+
+
+
+    public static void main(String[] args){
+        System.out.println("This is the Recursion II a power b problem!");
+    }
+}
--- src/main/java/com/company/SlidingWindow.java
+++ src/main/java/com/company/SlidingWindow.java
@@ -1,22 +1,130 @@
 package com.company;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class SlidingWindow {
     public static int longestSubstringWithoutDup(String input){
-        Set<Character> set = new HashSet<Character>();
-        //longestSubstring represents the length of the longest substring from index 0 to index i(i included);
-        int[] longestSubstring = new int[input.length()];
-        longestSubstring[0] = 1;
-        for(int i = 1; i<input.length();i++){
-            if(!set.contains(input.charAt(i)) ){
-                longestSubstring
+
+        Set<Character> set = new HashSet<>();
+        int slow = 0;
+        int fast = 0;
+        int globalLongest = 0;
+
+        //in the sliding window, it contains all the characters, with index slow included
+        while (fast<input.length()){
+            if(set.contains(input.charAt(fast))){
+                set.remove(input.charAt(slow++));
+            } else {
+                set.add(input.charAt(fast++));
+                globalLongest = Math.max(globalLongest, fast-slow);
             }
         }
+        return globalLongest;
+
 
     }
+    public static String longestAnagram(String s, String l){
+        //assumptions:  s, l are not null, l and s is not empty, s is shorter than l
+        List<Integer> result = new ArrayList<Integer>();
+        Map<Character, Integer> map = countMap(s);
+
+        int left = 0;
+
+        int match = 0;
+        int globalStart = 0;
+        int globalEnd = 0;
+
+        for(int right = 0; right < l.length();right++){
+            char tmp = l.charAt(right);
+            Integer count = map.get(tmp);
+            if(count != null){
+                map.put(tmp, count -1);
+                if(count == 1){
+                    match++;
+                } else if( count == 0){ //move left to make it a match
+                    continue;
+
+                }
+            }
+
+        }
+        return new String("123");
+    }
+
+
+    public static List<Integer> allAnagrams(String s, String l){
+
+        //assumption: s, l are not null, s is not empty
+
+        List<Integer> result = new ArrayList<Integer>();
+        if(l.length() == 0){
+            return result;
+        }
+
+
+        //when s is longer than l, there is no way any of the substring coulc be an anagra
+        if(s.length()>l.length()){
+            return result;
+        }
+
+        //use a map to record the distinct characters in s and the number of each characters
+        //when we get an instance of 'a' in l, we let count of 'a' decremented by 1
+        //and only when the count is from 1 to 0, we have a totally matched
+        Map<Character, Integer> map = countMap(s);
+
+        //use a flag to record how many characters match the short string
+        int match = 0;
+
+        for(int i = 0; i<l.length(); i ++){
+            //check whether the character is in the count map
+            char tmp = l.charAt(i);
+            Integer count = map.get(tmp);
+            if(count != null){
+                map.put(tmp, count -1);
+                if(count == 1){
+                    match++;
+                }
+            }
+
+            //handle pull out the left characters at the previous window
+            if(i>=s.length()){
+                tmp = l.charAt(i-s.length());
+                count = map.get(tmp);
+                if(count != null){
+                    map.put(tmp,count+1);
+                    if(count == 0){
+                        match--;
+                    }
+                }
+            }
+            //for the current sliding windwo, if all the distinct characters are matched, add to the reuslt
+            if(match == map.size()){
+                result.add(i-s.length()+1);
+            }
+        }
+        return result;
+
+    }
+
+    private static Map<Character,Integer> countMap(String s) {
+        Map<Character,Integer> map = new HashMap<Character,Integer>();
+        for(char ch: s.toCharArray()){
+            Integer count = map.get(ch);
+            if(count == null){
+                map.put(ch,1);
+            } else {
+                map.put(ch, count+1);
+            }
+        }
+        return map;
+    }
+
+
     public static void main(String[] args){
+
         System.out.println("The sets of sliding window problems");
+        System.out.println(longestSubstringWithoutDup("The sets of sliding window problems"));
+        System.out.println(allAnagrams("in", "Thesetsofslidingwindowproblems" ));
+
     }
 }
