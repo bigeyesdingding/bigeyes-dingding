4733d845d85fc5cdb4f1d4c89795e4ee1676e503
hannah-ding <dinghan1993@gmail.com>
2017-09-06 17:49:37 -0700

    nothing

---
+++ src/main/java/com/company/DecompressString.java
@@ -0,0 +1,94 @@
+package com.company;
+
+public class DecompressString {
+    public static String decompressI(String input){
+        if(input.isEmpty()){
+            return input;
+        }
+        char[] array = input.toCharArray();
+        //consider two different cases
+        //typeA: a0,a1,a2 case(the decoded string is shorter)
+        //typeB: a3,a4,a5..(the decoded string is longer
+        return decodeLong(array, decodeShort(array));
+    }
+    public static int  decodeShort(char[] input){
+        //work from left to right
+        int end = 0;
+        for(int i = 0; i<input.length; i +=2){
+            int digit = getDigit(input[i+1]);
+            if(digit>=0 && digit<=2){
+                for(int j =0; j<digit;j++){
+                    input[end++] = input[i];
+                }
+
+
+            } else {
+                //in this method, we don't take care of this case
+                input[end++] = input[i];
+                input[end++] = input[i+1];
+            }
+        }
+        System.out.println(end);
+        return end;
+    }
+
+    //the decode string is longer
+    private static String decodeLong(char[] input, int length){
+        int newLength = length;
+        for(int i = 0; i<length;i++){
+            int digit = getDigit(input[i]);
+            if(digit>2 && digit<=9){
+                newLength += digit-2;
+            }
+        }
+
+        char[] result = new char[newLength];
+        int end = newLength-1;
+        for(int i = length-1; i>=0;i--){
+            int digit = getDigit(input[i]);
+            if(digit>2 && digit<=9){
+                i--;
+                for(int j = 0; j<digit;j++ ){
+                    result[end--] = input[i];
+                }
+            } else {
+                //we have already take the shorter case
+                result[end--] = input[i];
+            }
+        }
+        return new String(result);
+    }
+
+    private static int getDigit(char digit){
+        return digit - '0';
+    }
+
+
+    //method2: using string builder to do it
+    public static String decompressII(String input){
+        //assumption: input is not null
+        char[] array = input.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i<array.length; i++ ){
+            char ch =array[i++];
+            int count = array[i] - '0';
+            for(int c = 0; c<count;c++){
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+
+
+    public static void main(String[] args){
+        System.out.println("Try to decompress a string");
+        /*
+        * Assumption:
+        * 1.the string is not null
+        * 2.the characters used are guranteed to be 'a' to 'z'
+        * 3.there are no adjacent repeated characters with length>9
+        * */
+        System.out.println(decompressI("a3b0c2"));
+        System.out.println(decompressII("a3b0c2"));
+    }
+}
---
+++ src/main/java/com/company/PermutationWithDuplicates.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class PermutationWithDuplicates {
+    public static List<String> permutation(String set){
+        List<String> result = new ArrayList<>();
+        char[] array = set.toCharArray();
+        if(set == null){
+            return result;
+        }
+        helper(result,array,0);
+        return result;
+    }
+
+    private static void helper(List<String> result, char[] array, int cur){
+        if(cur == array.length){//base case
+            result.add(new String(array));
+            return;
+        }
+        //use a set to record all the distinct elements we have added
+        Set<Character> used = new HashSet<Character>();
+        for (int i = cur;i<array.length;i++){
+            if(used.add(array[i])){
+                swap(array,i,cur);
+                //go to the next level, index + 1
+                helper(result, array, cur+1);
+                //don't forget to do the clear operation when backtracking
+                swap(array, i, cur);
+            }
+        }
+
+    }
+
+    private static void swap(char[] array, int left, int right){
+        char tmp = array[left];
+        array[left] = array[right];
+        array[right] = tmp;
+    }
+
+    public static void main(String[] args){
+        System.out.println("Do permutation with duplicates in the set");
+        System.out.println(permutation("aabc"));
+    }
+}
