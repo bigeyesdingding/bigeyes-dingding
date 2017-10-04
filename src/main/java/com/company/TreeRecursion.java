7cfafa227bf08bd7693468ba49c9bd76e69c4392
Ding Han <handing@evestemptation.com>
2017-10-03 23:21:00 -0700

    recursive II

---
+++ src/main/java/com/company/TreeRecursion.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class TreeRecursion {
+    /*
+    * 1.从下向上传递直
+    *
+    *
+    * */
+    public static int getHeight(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        int left = getHeight(root.left);
+        int right = getHeight(root.right);
+        return Math.max(left, right) + 1;
+    }
+
+    //how to store how many nodes in each node's left-subtree
+    public static  int leftSubTree(TreeNode root){
+
+
+
+    }
+    public static void main(String[] args){
+
+    }
+}
