8279f14d6bf81efdec004f6bcec2df9dc02bf3a1
Han Ding <handing@evestemptation.com>
2017-10-06 00:03:53 -0700

    dp I & II

---
+++ src/main/java/com/company/CommonElements.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class CommonElements {
+
+
+    /*
+    * Assumptions
+    * 1.time/space
+    * 2.sorted or not?
+    * 3.duplicates?
+    * 4.size of a1 vs a2
+    * */
+}
