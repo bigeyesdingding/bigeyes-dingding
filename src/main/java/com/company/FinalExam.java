01afa2dbc6ff13a595f8683e6d644f89c6e89094
Han Ding <handing@evestemptation.com>
2017-10-08 23:50:44 -0700

    dp I & II

---
+++ src/main/java/com/company/FinalExam.java
@@ -0,0 +1,45 @@
+package com.company;
+
+public class FinalExam {
+    /*
+    * Q1:Given a string, we can insert at most one empty space between each pair of adjacent letters.
+    *   Print all permutations of strings after insertions of empty spaces
+    *   Input: str[] ="ABC"
+    *   Output:
+    *       ABC
+    *       AB_C
+    *       A_BC
+    *       A_B_C
+    *
+    *
+    * Q2: CHeck if two nodes are cousins in a Binary Tree
+    * Given the bianry Tree and the two nodes say 'a' and 'b', determine the two nodes are cousins of each other or not.
+    * Two Nodes are cousins of each other if they are at same level and have different parents.
+    * Example:
+    *        6
+    *       / \
+    *      3   5
+    *     /\   /\
+    *    7  8 1  2
+    *
+    *    7 and 1, return TRUE
+    *    3 and 5, return False
+    *    7 and 5, return False
+    *
+    * Q3: How to cut/split the number into a minimum number of items
+    *  such that each item is equal to an integer's square value.
+    *  For example: 4 can be split to 1+1+1+1(4 items) or 2^2(1 item, which is the solution)
+    *  Return 1
+    *  10 can be split to two items 3^2 +1^2 (solution) or four items 2^2+2^2+1^2+1^2
+    *  Return 2
+    *
+    * Q4: Given an array of strings, find if all the strings can be chained to form a circle.
+    * Two string s1 and s2 can be chained, if the last letter of s1 is identical to first letter of s2.
+    * For example, "abc" and "cd" can be chained,
+    * "abc" and "dz" can not be chained.
+    * Example Input: arr[] = {"abc". "bbb", "baa", "aab"};
+    * Output: True,
+    * The given input string scan be chained to form a circle. The strings can be chained as "aaa"-"aab"-"bbb"-"baa".
+    *
+    * */
+}
